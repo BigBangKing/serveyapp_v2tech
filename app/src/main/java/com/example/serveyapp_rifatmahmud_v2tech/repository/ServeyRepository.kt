86783703a00ca22bdb07be4cce0a39bc0932e510
Rifat <rifatalmahmud0111@gmail.com>
2020-08-21 12:00:07 +0600

    init

--- app/src/main/java/com/example/serveyapp_rifatmahmud_v2tech/repository/ServeyRepository.kt
+++ app/src/main/java/com/example/serveyapp_rifatmahmud_v2tech/repository/ServeyRepository.kt
@@ -2,12 +2,16 @@ package com.example.serveyapp_rifatmahmud_v2tech.repository
 
 import android.app.Application
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.serveyapp_rifatmahmud_v2tech.data.pojo.schema2x
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 
 class ServeyRepository {
@@ -21,25 +25,45 @@ class ServeyRepository {
 
         Log.d("test", "required method executed")
 
-        val jsonObjectRequest =
-            JsonObjectRequest(Request.Method.GET, url, null, {
-                val gson = Gson()
 
-                Log.d("test", it.toString())
-
-                val myResults = gson.fromJson(it.toString(), schema2x::class.java)
-
-                Log.d("test", myResults.toString())
-
-                mResponse?.setValue(myResults.options)
-
-            }, {
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                mResponse?.setValue(response.toString())
+                Log.v("test", response)
+            },
+            {
                 it.printStackTrace()
-                Log.d("test", "error happened.")
+                Toast.makeText(application, "Could not get data.", Toast.LENGTH_LONG).show()
+                Log.v("test", "response is nothing")
             })
 
-        queue.add(jsonObjectRequest)
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
 
+
+        /* val jsonObjectRequest =
+             JsonObjectRequest(Request.Method.GET, url, null, {
+                 val gson = Gson()
+
+                 Log.d("test", it.toString())
+
+                 val responseListType: Type? = object : TypeToken<ArrayList<schema2x?>?>() {}.type
+
+                 val userArray: schema2x? = gson.fromJson(it.toString(), schema2x::class.java)
+
+ //                val jsonarray = JSONArray(strResponse)
+ //                val myResults = gson.fromJson(it.toString(), schema2x::class.java)
+ //                Log.d("test", myResults.toString())
+ //                mResponse?.setValue(myResults.options)
+
+             }, {
+                 it.printStackTrace()
+                 Log.d("test", "error happened.")
+             })
+
+         queue.add(jsonObjectRequest)
+ */
     }
 
     fun getmResponse(): MutableLiveData<String>? {
